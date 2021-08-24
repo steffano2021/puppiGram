a8cf16edfb50d2c351bfcb7b7ef233542a25f7f9
steffano <steffano.espinoza10@gmail.com>
2021-08-24 15:02:54 -0700

    moved the app migrate on the init

--- app/__init__.py
+++ app/__init__.py
@@ -29,10 +29,10 @@ def load_user(id):
 app.cli.add_command(seed_commands)
 
 app.config.from_object(Config)
-app.register_blueprint(user_routes, url_prefix='/api/users')
-app.register_blueprint(auth_routes, url_prefix='/api/auth')
 db.init_app(app)
 Migrate(app, db)
+app.register_blueprint(user_routes, url_prefix='/api/users')
+app.register_blueprint(auth_routes, url_prefix='/api/auth')
 
 # Application Security
 CORS(app)
