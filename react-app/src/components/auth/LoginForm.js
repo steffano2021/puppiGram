7c0fde8ec46082e77806f766651b21bce204255d
steffano <steffano.espinoza10@gmail.com>
2021-08-26 02:10:11 -0700

    login and signup css done, just need error handling

--- react-app/src/components/auth/LoginForm.js
+++ react-app/src/components/auth/LoginForm.js
@@ -1,9 +1,12 @@
 import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { Redirect } from 'react-router-dom';
+import { Redirect, useHistory } from 'react-router-dom';
 import { login } from '../../store/session';
 
 const LoginForm = () => {
+
+  const history = useHistory();
+
   const [errors, setErrors] = useState([]);
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
@@ -31,34 +34,48 @@ const LoginForm = () => {
   }
 
   return (
-    <form onSubmit={onLogin}>
-      <div>
-        {errors.map((error, ind) => (
-          <div key={ind}>{error}</div>
-        ))}
+    <div className='auth_background'>
+    <div className='auth_container'>
+      <img className='auth-photo' src='https://i.imgur.com/pOT9xDs.jpg' alt='signup-photo' />
+      <div className='auth-photo-text'>
+        <p>Glad you're back</p>
+        <p>Let's see what you missed out</p>
       </div>
-      <div>
-        <label htmlFor='email'>Email</label>
-        <input
+      <div className='auth_form_container'>
+      <form onSubmit={onLogin}>
+        <div className='auth-form-logo'>
+            <div>puppiGram</div>
+            <div>Log in to your account</div>
+        </div>
+        <div className='auth-form-input_container'>
+          <i class="fas fa-envelope" />
+          <input
           name='email'
           type='text'
-          placeholder='Email'
+          placeholder='email'
           value={email}
           onChange={updateEmail}
-        />
-      </div>
-      <div>
-        <label htmlFor='password'>Password</label>
-        <input
+          />
+        </div>
+        <div className='auth-form-input_container'>
+          <i class="fas fa-key" />
+          <input
           name='password'
           type='password'
-          placeholder='Password'
+          placeholder='password'
           value={password}
           onChange={updatePassword}
-        />
-        <button type='submit'>Login</button>
-      </div>
-    </form>
+          />
+        </div>
+        <div className='auth-form-btn_container'>
+          <button type='submit'>Login</button>
+          <button type='button' onClick={() => history.push('/signup')} >Sign up</button>
+          <button type='button' onClick={() => history.push('/')} >Cancel</button>
+        </div>
+      </form>
+    </div>
+    </div>
+    </div>
   );
 };
 
